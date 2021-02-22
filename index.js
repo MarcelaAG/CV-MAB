ce2be1100df2b7f1e056628fb7d3643bcf278ce9
MarcelaAG <marcela.aguilerablanc@gmail.com>
2021-02-22 11:02:34 +0100

    changes from 19 Feb

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//my head hurts. This was not easy to understand but I think I got it
+
 const navToggle = document.querySelector('.nav-toggle') // set up to open nav when button is clicked
 const navLinks = document.querySelectorAll('.nav__link') // set up to  take user to desired section and close nav
 
